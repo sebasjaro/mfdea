249da83cacb9a247a417666c08f4804d8f2c8be9
sebasjaro <sebasjaro@gmail.com>
2022-05-17 08:11:56 -0300

    Se optimizo el algoritmo DEA

--- mfdea/mfdea.py
+++ mfdea/mfdea.py
@@ -1,36 +1,48 @@
-"""Main module."""
-
 import numpy as np
-from collections import Counter
+import matplotlib.pyplot as plt
 
-def proba(xl):
-   prob = Counter(xl).most_common()
-   return(prob)
+def renyi(p,q):
+   return np.log(np.sum(p**q))/(1-q)
 
-def entro(x):
-   p = proba(x)
-   lonx = len(x)
-   lonp = len(p)
-   pi = np.zeros(lonp)
-   for i in range(lonp):
-      pi[i] = (p[i][1])/lonx
-   lpi = np.log(pi)
-   return(-1. *  np.inner(pi,lpi))
+def proba(x, size):
+    #bins = int(len(x)/size)
+    bins = (np.max(x)-np.min(x))/size
+    h1, h2 = np.histogram(x, bins = int(bins))
+    nozero = np.nonzero(h1)[0]
+    p = [h1[w] for w in nozero]
+    return np.array(p)
 
-test = np.random.randint(1,3,20)
-test = np.array([1,1,-1,1,-1])
+def dea(array, si, sf, file, n):
+   N = len(array)
+   ts = np.arange(si,sf,10)
+   size = np.std(test)/n
+   for t in ts:
+       n_traje = N - t + 1
+       xs = np.zeros(n_traje)
+       xs = [np.sum(array[s:t+s]) for s in range(n_traje)]
+       prob = proba(xs, size)/n_traje
+       file.write(str(t) + ' ' + str(-1 * np.inner(prob,np.log(prob))) + ' ' + str(renyi(prob,5)) + '\n')
+
+
+
+#np.random.seed(100)
+#test = np.random.randint(1,3,1000)
+#test = np.array([1,1,-1,1,-1,])
+#test = np.loadtxt('./datos/walk_short_eco110k.gb')
+
+from fbm import FBM 
+f = FBM(n=50000, hurst=0.75, length=1, method='daviesharte')
+test = f.fgn()
+
+import pandas as pd
+from scipy.stats.kde import gaussian_kde
+
 N = len(test)
+si = 1
+sf = 300
+valores = [.5,1,1.5,2,2.5,3,3.5,4,4.5,5]
+for val in valores:
+    file = open('salida_dea'+'_'+str(val)+'.dat','w')
+    dea(test, si, sf, file, val)
+    file.close()
 
-ts = np.arange(1,6)
-
-for t in ts:
-    n_traje = N - t + 1  # numero de secuencias para cada t
-    xs = np.zeros(n_traje, dtype=int)
-    for s in range(n_traje):
-        for i in range(t):
-            xs[s] = xs[s] + test[i+s]
-    print(t, entro(xs))
-
-        
-
-    
